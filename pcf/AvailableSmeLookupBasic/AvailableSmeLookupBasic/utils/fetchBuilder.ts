684d42d49f6e2d0cbbb8c0603d441de8b39453ab
Blake Bradford <b@blrfam.win>
2025-02-11 15:27:39 -0500

    Add fetchOverlappingSmeRequestsFetchBuilder function to construct fetch XML for overlapping SME requests

--- pcf/AvailableSmeLookupBasic/AvailableSmeLookupBasic/utils/fetchBuilder.ts
+++ pcf/AvailableSmeLookupBasic/AvailableSmeLookupBasic/utils/fetchBuilder.ts
@@ -1,3 +1,5 @@
+import { SmeRequest } from "../types/SmeRequest";
+
 export const filterForAvailableSmeFetchBuilder = (
   publisherPrefix: string,
   resourceRequestId: string
@@ -55,5 +57,94 @@ export const fetchResourceRequestFetchBuilder = (
         </fetch>
         `;
   //console.debug("fetchResourceRequestFetchBuilder", fetchXml);
+  return fetchXml;
+};
+
+export const fetchOverlappingSmeRequestsFetchBuilder = (
+  smeRequest: SmeRequest,
+  selectedSme: ComponentFramework.LookupValue
+): string => {
+  const fetchXml = `<fetch>
+        <entity name="${smeRequest.publisherPrefix}_smerequest">
+            <attribute name="${smeRequest.publisherPrefix}_smerequestid" />
+            <attribute name="${smeRequest.publisherPrefix}_autoid" />
+            <attribute name="${smeRequest.publisherPrefix}_startdate" />
+            <attribute name="${smeRequest.publisherPrefix}_enddate" />
+            <filter type="or">
+                <filter>                
+                    <condition 
+                        attribute="${smeRequest.publisherPrefix}_startdate"
+                        operator="ge"
+                        value="${smeRequest.startDate}"
+                    />
+                    <condition
+                        attribute="${smeRequest.publisherPrefix}_startdate"
+                        operator="le"
+                        value="${smeRequest.endDate}"
+                    />
+                </filter>                
+                <filter>
+                    <condition
+                        attribute="${smeRequest.publisherPrefix}_enddate"
+                        operator="ge"
+                        value="${smeRequest.startDate}"
+                    />
+                    <condition
+                        attribute="${smeRequest.publisherPrefix}_enddate"
+                        operator="le"
+                        value="${smeRequest.endDate}"
+                    />
+                </filter>                
+                <filter>
+                    <condition
+                        attribute="${smeRequest.publisherPrefix}_startdate"
+                        operator="le"
+                        value="${smeRequest.startDate}"
+                    />
+                    <condition
+                        attribute="${smeRequest.publisherPrefix}_enddate"
+                        operator="ge"
+                        value="${smeRequest.endDate}"
+                    />
+                </filter>                
+                <filter>
+                    <condition
+                    attribute="${smeRequest.publisherPrefix}_startdate"
+                    operator="ge"
+                    value="${smeRequest.startDate}"
+                />
+                    <condition
+                        attribute="${smeRequest.publisherPrefix}_enddate"
+                        operator="le"
+                        value="${smeRequest.endDate}"
+                    />
+                </filter>                    
+            </filter>
+            <filter>
+                <condition
+                    attribute="${smeRequest.publisherPrefix}_smerequestid"
+                    operator="ne"
+                    value="${smeRequest.id}"
+                />
+            </filter>
+            <link-entity
+                name="${smeRequest.publisherPrefix}_sme"
+                from="${smeRequest.publisherPrefix}_smeid"
+                to="${smeRequest.publisherPrefix}_requestedsme"
+                alias="sme"
+            >
+                    <attribute name="${smeRequest.publisherPrefix}_smeid" />
+                    <attribute name="${smeRequest.publisherPrefix}_email" />
+                    <attribute name="${smeRequest.publisherPrefix}_name" />
+                <filter>
+                    <condition
+                        attribute="${smeRequest.publisherPrefix}_email"
+                        operator="eq"
+                        value="${selectedSme.id}"
+                    />
+                </filter>
+            </link-entity>
+        </entity>
+    </fetch>`;
   return fetchXml;
 };

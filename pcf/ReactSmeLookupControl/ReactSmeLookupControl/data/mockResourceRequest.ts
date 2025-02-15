2479d5437f06195a5c77b8c15f9d80ae6a62fb41
Blake Bradford <b@blrfam.win>
2025-02-15 17:29:53 -0500

    Add mock data for assigned SME resource requests

---
+++ pcf/ReactSmeLookupControl/ReactSmeLookupControl/data/mockResourceRequest.ts
@@ -0,0 +1,13 @@
+export const mockAssignedSmeData = {
+  value: [
+    {
+      "@odata.etag": 'W/"2386409"',
+      pmt_autoid: "1000",
+      pmt_resourcerequestid: "72c6fc2f-e3eb-ef11-be20-7c1e5248a34a",
+      pmt_Program: {
+        pmt_name: "A program name",
+        pmt_programid: "ac6cbce7-e2eb-ef11-be20-7c1e5248a34a",
+      },
+    },
+  ],
+};

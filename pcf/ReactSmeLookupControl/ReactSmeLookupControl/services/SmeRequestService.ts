b5d910c3b8de9cb5b24b53e2341833ba27937b01
Blake Bradford <b@blrfam.win>
2025-02-15 17:27:49 -0500

    Add SmeRequestService to handle SME request retrieval from the web API

---
+++ pcf/ReactSmeLookupControl/ReactSmeLookupControl/services/SmeRequestService.ts
@@ -0,0 +1,30 @@
+import { IInputs } from "../generated/ManifestTypes";
+import { ISmeRequestApiResult, SmeRequest } from "../types/SmeRequest";
+
+export class SmeRequestService {
+  private _context: ComponentFramework.Context<IInputs>;
+  private _options = `$select=pmt_autoid,pmt_smerequestid&$expand=pmt_AssignedSME($select=pmt_assignedsmeid,pmt_email,pmt_fullname;$expand=pmt_Facility($select=pmt_facilityid,pmt_pmtfacilityid)),pmt_Request($select=pmt_autoid,pmt_resourcerequestid;$expand=pmt_Program($select=pmt_name,pmt_programid)),pmt_RequestedSME($select=pmt_assignedsmeid,pmt_email,pmt_fullname;$expand=pmt_Facility($select=pmt_facilityid,pmt_pmtfacilityid))`;
+
+  constructor(context: ComponentFramework.Context<IInputs>) {
+    this._context = context;
+  }
+
+  public async getSmeRequests(): Promise<SmeRequest[]> {
+    const smeRequests = await this._context.webAPI.retrieveMultipleRecords(
+      "pmt_smerequest",
+      this._options
+    );
+    return smeRequests.entities.map((entity) =>
+      SmeRequest.fromJson(entity as ISmeRequestApiResult)
+    );
+  }
+
+  public async getSmeRequest(id: string): Promise<SmeRequest> {
+    const smeRequest = await this._context.webAPI.retrieveRecord(
+      "pmt_smerequest",
+      id,
+      this._options
+    );
+    return SmeRequest.fromJson(smeRequest as ISmeRequestApiResult);
+  }
+}

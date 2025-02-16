08fad60e8568fe6dc941a6ae3524c2d09ca4a2ef
Blake Bradford <b@blrfam.win>
2025-02-16 14:46:38 -0500

    Fix selectedSme state handling in Container component and add debug logging

--- pcf/ReactSmeLookupControl/ReactSmeLookupControl/components/Container.tsx
+++ pcf/ReactSmeLookupControl/ReactSmeLookupControl/components/Container.tsx
@@ -19,6 +19,7 @@ export const Container = (props: IContainerProps) => {
   const [assignedSmes, setAssignedSmes] = React.useState<AssignedSme[]>([]);
   const [selectedSme, setSelectedSme] =
     React.useState<ComponentFramework.LookupValue | null>(selectedItem ?? null);
+  console.debug("Container.SelectedSme: ", selectedSme);
 
   React.useEffect(() => {
     if (environment === "DEV") {
@@ -28,7 +29,7 @@ export const Container = (props: IContainerProps) => {
       });
       setAssignedSmes(smes);
 
-      if (!selectedItem?.id && smes.length > 0 && isLoading) {
+      if (!selectedSme?.id && smes.length > 0 && isLoading) {
         console.debug("Setting selectedSme: ", smes[0]);
         setSelectedSme({
           id: smes[0].id,
@@ -50,6 +51,7 @@ export const Container = (props: IContainerProps) => {
           console.error("Error fetching assigned smes: ", error);
           setIsLoading(false);
         });
+      setSelectedSme(selectedItem);
     }
   }, [selectedItem, environment, context]);
 

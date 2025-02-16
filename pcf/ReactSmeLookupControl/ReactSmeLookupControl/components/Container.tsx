ceb8021d91f992fdbba0dfd8dd36905317d6e499
Blake Bradford <b@blrfam.win>
2025-02-16 09:32:47 -0500

    Add Container component to manage loading state and assigned SMEs in ReactSmeLookupControl

---
+++ pcf/ReactSmeLookupControl/ReactSmeLookupControl/components/Container.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import { IInputs } from "../generated/ManifestTypes";
+import { AssignedSme, IAssignedSmeApiResult } from "../types/AssignedSme";
+import { mockAssignedSmeData } from "../data/mockAssignedSme";
+import { AssignedSmeService } from "../services/AssignedSmeService";
+import { SmeLookup } from "./SmeLookup";
+
+export interface IContainerProps {
+  context: ComponentFramework.Context<IInputs>;
+  environment: string;
+  selectedItem: ComponentFramework.LookupValue | null;
+  onInputChange: (sme: ComponentFramework.LookupValue | null) => void;
+}
+
+export const Container = (props: IContainerProps) => {
+  const { context, environment, selectedItem, onInputChange } = props;
+
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [assignedSmes, setAssignedSmes] = React.useState<AssignedSme[]>([]);
+  const [selectedSme, setSelectedSme] =
+    React.useState<ComponentFramework.LookupValue | null>(selectedItem ?? null);
+
+  React.useEffect(() => {
+    if (environment === "DEV") {
+      console.debug("Using mock data for assigned smes");
+      const smes = mockAssignedSmeData.value.map((item) => {
+        return AssignedSme.fromJson(item as IAssignedSmeApiResult);
+      });
+      setAssignedSmes(smes);
+
+      if (!selectedItem?.id && smes.length > 0 && isLoading) {
+        console.debug("Setting selectedSme: ", smes[0]);
+        setSelectedSme({
+          id: smes[0].id,
+          name: smes[0].email,
+          entityType: "pmt_assignedsme",
+        });
+      }
+      setIsLoading(false);
+    } else {
+      const assignedSmeService = new AssignedSmeService(context);
+      assignedSmeService
+        .getAssignedSmes()
+        .then((data) => {
+          setAssignedSmes(data);
+          if (selectedItem === null && data.length > 0) {
+            setSelectedSme({
+              id: data[0].id,
+              name: data[0].email,
+              entityType: "pmt_assignedsme",
+            });
+          }
+          setIsLoading(false);
+          return;
+        })
+        .catch((error) => {
+          console.error("Error fetching assigned smes: ", error);
+          setIsLoading(false);
+        });
+    }
+  }, [selectedItem, environment, context]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <SmeLookup
+      assignedSmes={assignedSmes}
+      selectedItem={selectedSme}
+      onInputChange={(sme) => {
+        setSelectedSme(sme);
+        onInputChange(sme);
+      }}
+    />
+  );
+};

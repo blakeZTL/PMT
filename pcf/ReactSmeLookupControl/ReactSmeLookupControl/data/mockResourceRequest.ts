502ed1233555f985fa2c74e73d0ff138801f7364
Blake Bradford <b@blrfam.win>
2025-02-16 17:01:44 -0500

    Rename mockAssignedSmeData to mockResourceRequest and add sample resource request data

--- pcf/ReactSmeLookupControl/ReactSmeLookupControl/data/mockResourceRequest.ts
+++ pcf/ReactSmeLookupControl/ReactSmeLookupControl/data/mockResourceRequest.ts
@@ -1,4 +1,4 @@
-export const mockAssignedSmeData = {
+export const mockResourceRequest = {
   value: [
     {
       "@odata.etag": 'W/"2386409"',
@@ -8,6 +8,16 @@ export const mockAssignedSmeData = {
         pmt_name: "A program name",
         pmt_programid: "ac6cbce7-e2eb-ef11-be20-7c1e5248a34a",
       },
+      pmt_ResourceRequest_pmt_AssignedSME_pmt_AssignedSME: [
+        {
+          "@odata.etag": 'W/"2484381"',
+          pmt_assignedsmeid: "ca3463b4-83ec-ef11-be20-7c1e5267196e",
+        },
+        {
+          "@odata.etag": 'W/"2484317"',
+          pmt_assignedsmeid: "2c470fc0-83ec-ef11-be20-7c1e5267196e",
+        },
+      ],
     },
   ],
 };

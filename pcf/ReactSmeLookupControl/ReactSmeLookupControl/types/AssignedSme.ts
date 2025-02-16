a018ce83bc3ee7c4d2c70259ea21341f7d1435c6
Blake Bradford <b@blrfam.win>
2025-02-16 18:11:42 -0500

    Add support for simplified SME requests with hours in AssignedSme model

--- pcf/ReactSmeLookupControl/ReactSmeLookupControl/types/AssignedSme.ts
+++ pcf/ReactSmeLookupControl/ReactSmeLookupControl/types/AssignedSme.ts
@@ -8,6 +8,7 @@ export interface IAssignedSmeApiResult {
     pmt_facilityid: string;
     pmt_pmtfacilityid: string;
   };
+  pmt_smerequest_AssignedSME_pmt_assignedsme: ISimplifiedSmeRequestWithHours[];
 }
 
 export interface IFacility {
@@ -15,17 +16,32 @@ export interface IFacility {
   pmt_pmtfacilityid: string;
 }
 
+export interface ISmeHour {
+  pmt_date: string;
+  pmt_autoid: string;
+  pmt_smehourid: string;
+  pmt_hours: number;
+}
+
+export interface ISimplifiedSmeRequestWithHours {
+  pmt_autoid: string;
+  pmt_smerequestid: string;
+  pmt_smehour_Request_pmt_smerequest: ISmeHour[];
+}
+
 export class AssignedSme {
   public id: string;
   public fullName: string;
   public email: string;
   public facility: { facilityId: string; id: string };
+  public smeRequests: ISimplifiedSmeRequestWithHours[];
 
   constructor(
     id?: string,
     fullName?: string,
     email?: string,
-    facility?: IFacility
+    facility?: IFacility,
+    smeRequests?: ISimplifiedSmeRequestWithHours[]
   ) {
     this.id = id ?? "";
     this.fullName = fullName ?? "";
@@ -34,6 +50,7 @@ export class AssignedSme {
       facilityId: facility?.pmt_facilityid ?? "",
       id: facility?.pmt_pmtfacilityid ?? "",
     };
+    this.smeRequests = smeRequests ?? [];
   }
 
   public static fromJson(json: IAssignedSmeApiResult): AssignedSme {
@@ -51,6 +68,32 @@ export class AssignedSme {
       pmtFacilityId = json.pmt_Facility.pmt_pmtfacilityid;
     }
 
+    const smeRequests: ISimplifiedSmeRequestWithHours[] = [];
+    if (json.pmt_smerequest_AssignedSME_pmt_assignedsme) {
+      const assignedSmeRequests =
+        json.pmt_smerequest_AssignedSME_pmt_assignedsme;
+
+      assignedSmeRequests.forEach((item) => {
+        if (item.pmt_smehour_Request_pmt_smerequest?.length === 0) {
+          return;
+        }
+        const smeHours: ISmeHour[] = [];
+        item.pmt_smehour_Request_pmt_smerequest.forEach((smeHour) => {
+          smeHours.push({
+            pmt_date: smeHour.pmt_date,
+            pmt_autoid: smeHour.pmt_autoid,
+            pmt_smehourid: smeHour.pmt_smehourid,
+            pmt_hours: smeHour.pmt_hours,
+          });
+        });
+        smeRequests.push({
+          pmt_autoid: item.pmt_autoid,
+          pmt_smerequestid: item.pmt_smerequestid,
+          pmt_smehour_Request_pmt_smerequest: smeHours,
+        });
+      });
+    }
+
     return new AssignedSme(
       json.pmt_assignedsmeid,
       json.pmt_fullname,
@@ -58,7 +101,8 @@ export class AssignedSme {
       {
         pmt_facilityid: facilityId,
         pmt_pmtfacilityid: pmtFacilityId,
-      }
+      },
+      smeRequests
     );
   }
 }

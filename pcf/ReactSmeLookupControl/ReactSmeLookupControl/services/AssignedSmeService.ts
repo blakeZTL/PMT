b06216fe0ee7e6acbfd59a7eb368d6efea6cbbe9
Blake Bradford <b@blrfam.win>
2025-02-16 21:02:31 -0500

    Refactor AssignedSmeService options to simplify query parameters

--- pcf/ReactSmeLookupControl/ReactSmeLookupControl/services/AssignedSmeService.ts
+++ pcf/ReactSmeLookupControl/ReactSmeLookupControl/services/AssignedSmeService.ts
@@ -3,7 +3,7 @@ import { AssignedSme, IAssignedSmeApiResult } from "../types/AssignedSme";
 
 export class AssignedSmeService {
   private _context: ComponentFramework.Context<IInputs>;
-  private _options = `?$select=pmt_assignedsmeid,pmt_email,pmt_fullname&$expand=pmt_Facility($select=pmt_facilityid,pmt_pmtfacilityid),pmt_smerequest_AssignedSME_pmt_assignedsme($select=pmt_autoid,pmt_smerequestid;$expand=pmt_smehour_Request_pmt_smerequest($select=pmt_autoid,pmt_date,pmt_hours,pmt_smehourid);$filter=(pmt_smerequestid ne 0675013b-e3eb-ef11-be20-7c1e5248a34a))&$filter=(pmt_assignedsmeid eq 8c1586df-d6eb-ef11-be20-7c1e5248a34a)`;
+  private _options = `?$select=pmt_assignedsmeid,pmt_email,pmt_fullname&$expand=pmt_Facility($select=pmt_facilityid,pmt_pmtfacilityid),pmt_smerequest_AssignedSME_pmt_assignedsme($select=pmt_autoid,pmt_smerequestid;$expand=pmt_smehour_Request_pmt_smerequest($select=pmt_autoid,pmt_date,pmt_hours,pmt_smehourid);)`;
 
   constructor(context: ComponentFramework.Context<IInputs>) {
     this._context = context;
